93ecf3ed3662981b687a574751f1bfceffc8f26b
Henrique Ribeiro <Henriquecarri2@gmail.com>
2021-04-10 21:27:56 -0300

    bot

---
+++ rossmann-telegram-bot/rossmann-telegram-bot.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import requests
+import json
+from flask import Flask, request, Response
+
+#constants
+token = '1730000057:AAFZ7zMcq7KomedEFzCk8ceCgZX_Hk6TTlk'
+#https://api.telegram.org/bot1730000057:AAFZ7zMcq7KomedEFzCk8ceCgZX_Hk6TTlk
+
+#info about the bot
+#https://api.telegram.org/bot1730000057:AAFZ7zMcq7KomedEFzCk8ceCgZX_Hk6TTlk/getMe
+
+#get Webhook
+#https://api.telegram.org/bot1730000057:AAFZ7zMcq7KomedEFzCk8ceCgZX_Hk6TTlk/setWebhook?url=https://f7e8c155be4ecb.localhost.run
+
+#get update
+#https://api.telegram.org/bot1730000057:AAFZ7zMcq7KomedEFzCk8ceCgZX_Hk6TTlk/getUpdates
+
+def send_message(chat_id,text):
+    url = 'https://api.telegram.org/bot{}/'.format(token)
+    url = url + 'sendMessage?chat_id={}'.format(chat_id)
+    r = requests.post(url, json={'text': text})
+    print('Status Code {}'.format(r.status_code))
+
+    return None
+
+def load_dataset(store_id):
+    #loading test dataset
+    df10 = pd.read_csv('C:/Users/Henrique/data_science_producao/test.csv')
+    df_store_raw = pd.read_csv('C:/Users/Henrique/data_science_producao/store.csv')
+
+    #merge test dataset + store
+    df_test = pd.merge(df10, df_store_raw, how='left', on='Store')
+
+    #choose store for prediction
+    df_test = df_test[df_test['Store'] == store_id]
+
+    if not df_test.empty:
+        #remove closed days
+        df_test = df_test[df_test['Open'] != 0]
+        df_test = df_test[~df_test['Open'].isnull()]
+        df_test = df_test.drop('Id', axis=1)
+
+        #convert dataframe to json
+        data = json.dumps(df_test.to_dict(orient='records'))
+
+    else:
+        data = 'error'
+
+    return data
+
+def predict(data):
+    #API call
+    url = 'https://rossmann-modelv1.herokuapp.com/rossmann/predict'
+    header = {'Content-type':'application/json'}
+    data = data
+
+    r = requests.post(url, data=data, headers=header)
+    print('Status Code {}'.format(r.status_code))
+    d1 = pd.DataFrame(r.json(),columns=r.json()[0].keys())
+
+    return d1
+
+def parse_message(message):
+    chat_id = message['message']['chat']['id']
+    store_id = message['message']['text']
+
+    store_id = store_id.replace('/','')
+
+    try:
+        store_id = int(store_id)
+
+    except ValueError:
+        send_message(chat_id, 'StoreID is wrong')
+        store_id = 'error'
+
+    return chat_id, store_id
+
+#API initialize
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method.get_json():
+        message = request.get_json()
+
+        chat_id, store_id = parse_message(message)
+
+        if store_id != 'error':
+            #loading data
+            data = load_dataset(store_id)
+
+            if data != 'error':
+                #predction
+                d1 = predict(data)
+
+                #calculation
+                d2 = d1[['store', 'prediction']].groupby('store').sum().reset_index()
+
+                #send message
+                msg = f'Store Number {d2["store"].values[0]} will sell $: {d2["prediction"].values[0]:, .2f} in the next 6 months'
+
+            else:
+                send_message(chat_id, 'Store Not Available')
+                return Response('OK', status=200)
+
+        else:
+            send_message(chat_id,'Store ID is wrong')
+            return Response('OK', status=200)
+
+    else:
+        return '<h1> Rossmann Telegram BOT <h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
